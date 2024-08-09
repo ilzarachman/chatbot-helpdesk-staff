06eed2aec9ed862df2b7e90179655a52b06ba6cd
ilzarachman <ilzarachman2@gmail.com>
2024-08-09 12:51:23 +0700

    feat: Update columns configurations

--- components/documents/list-document/columns.tsx
+++ components/documents/list-document/columns.tsx
@@ -66,7 +66,7 @@ export const columns: ColumnDef<Document>[] = [
         accessorKey: "public",
         header: ({ column }) => {
             return (
-                <div className="flex items-center">
+                <div className="flex items-center justify-center">
                     Public
                     <Button
                         variant="ghost"
@@ -77,7 +77,17 @@ export const columns: ColumnDef<Document>[] = [
                     </Button>
                 </div>
             )
-        }
+        },
+        cell: (info) => {
+            return info.getValue() ?
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-green-500 mx-auto">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                :
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-red-500 mx-auto">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+        },
     },
     {
         accessorKey: "embedded",
